4355d5d81dd95ad13ddcbb560e49e959f6069062
20030065 <wangsonghao@shinho.net.cn>
2020-10-10 17:03:16 +0800

    test

--- src/main/java/com/mycompany/myapp/repository/WsStoreRepository.java
+++ src/main/java/com/mycompany/myapp/repository/WsStoreRepository.java
@@ -6,9 +6,11 @@ import org.hibernate.annotations.SQLUpdate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -21,9 +23,13 @@ import java.util.List;
 public interface WsStoreRepository extends JpaRepository<WsStore, Long> {
     Page<WsStore> findAllByAreaId(Long areaId, Pageable pageable);
 
-    @Query(value = "update ws_store set balance=balance+ :money where id =:id", nativeQuery = true)
+    @Transactional
+    @Modifying
+    @Query(value = "update WsStore set balance=balance + ?2 where id = ?1")
     void addBalance(@Param("id") Long id, @Param("money") BigDecimal money);
 
-    @Query(value = "update ws_store set balance=balance- :money where id =:id", nativeQuery = true)
+    @Transactional
+    @Modifying
+    @Query(value = "update WsStore set balance=balance - ?2 where id = ?1")
     void subBalance(@Param("id") Long id, @Param("money") BigDecimal money);
 }
